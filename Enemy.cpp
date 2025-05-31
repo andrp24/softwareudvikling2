4ff22035e7c7a00d788ee62527d6280db0d3c22f
andrp24 <andrp24@student.sdu.dk>
2025-05-31 15:51:36 +0200

    Update Enemy.cpp
    simplificered og lavet så hp reseter efter en fight og man kan nu bruge lvl til enten få 2 strength eller 5 hp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,21 +1,4 @@
 #include "Enemy.h"
-#include <iostream>
 
-Enemy::Enemy(const std::string& name, int hp, int damage, int xp)
-    : name(name), hp(hp), damage(damage), xp(xp) {}
-
-const std::string& Enemy::getName() const { return name; }
-int Enemy::getHP() const { return hp; }
-int Enemy::getDamage() const { return damage; }
-int Enemy::getXP() const { return xp; }
-bool Enemy::isAlive() const { return hp > 0; }
-
-void Enemy::takeDamage(int dmg) {
-    hp -= dmg;
-    if (hp < 0) hp = 0;
-}
-
-void Enemy::printStatus() const {
-    std::cout << name << " (HP: " << hp << ", Damage: " << damage << ", XP: " << xp << ")\n";
-}
-
+Enemy::Enemy(std::string n, int h, int s, int xp, int gold)
+    : name(n), hp(h), strength(s), xpReward(xp), goldReward(gold) {}

97eea727461f89c1b613cb304c7f9f386f2f4826
andrp24 <andrp24@student.sdu.dk>
2025-06-01 14:08:48 +0200

    2/3 iteration
    tilføjet bonus guld når der besejres en dungeon

--- Dungeon.h
+++ Dungeon.h
@@ -1,22 +1,21 @@
 #ifndef DUNGEON_H
 #define DUNGEON_H
 
-#include "Hero.h"
-#include "Enemy.h"
 #include <vector>
 #include <string>
+#include "Enemy.h"
+#include "Hero.h"
 
 class Dungeon {
 public:
-    Dungeon(std::string name, std::vector<Enemy> enemies);
-
+    Dungeon(std::string name, std::vector<Enemy> enemies, int bonusGold);
     void enter(Hero& hero);
     std::string getName() const;
 
 private:
     std::string name;
     std::vector<Enemy> enemies;
+    int bonusGold;
 };
 
 #endif
-
